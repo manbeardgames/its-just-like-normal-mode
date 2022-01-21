fb408fab139ffbf31aa15d977f9477d1b0ab5022
Christopher Whitley <manbeardgames@gmail.com>
2022-01-20 21:35:21 -0500

    Added fledgling flight doc

--- source/its-just-like-normal-mode/gulpfile.js
+++ source/its-just-like-normal-mode/gulpfile.js
@@ -9,7 +9,7 @@ const files = './static/img/pandaemonium-asphodelos/**/*';
 const output = './minification/';
 
 function minimg() {
-    return src('./static/img/pandaemonium-asphodelos/**/*')
+    return src('./static/img/pandaemonium-asphodelos/the-third-circle-not-min/**/*')
         .pipe(webp({quality: 50}))
         .pipe(dest(output));
 }
